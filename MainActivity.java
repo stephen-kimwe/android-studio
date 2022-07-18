4b1257a7e7dc06faeb5da255c68fa070f00dd63e
Stephen Kimwe <stephenkimwe96@gmail.com>
2022-07-18 17:15:48 +0300

    Update MainActivity.java
    task1

--- MainActivity.java
+++ MainActivity.java
@@ -1,15 +1,26 @@
-##reward app;
+reward app;
 
-##item 1, price, discount
-##item 2, price, discount
-##item 3, price, discount
-##item 4, price, discount
+/** create 4 buttons
+item 1, price, discount
+item 2, price, discount
+item 3, price, discount
+item 4, price, discount
+**/
   
-##read customer ID
-##read items bought
+/**
+read customer ID
+read items bought
+**/
 
-##Calculate total items and total price for each item
-##Calculate discount
+/**
+Calculate total items and total price for each item
+Calculate discount
+**/
 
-##if four different items or more and price is above 1000 shortlist
-##open next activity display winner
+/**
+if four different items or more and price is above 1000 shortlist
+**/
+
+/**
+open next activity display winner
+**/
