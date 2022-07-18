3e4641d472370326c9b42131d5d23d3f0d3649cc
Stephen Kimwe <stephenkimwe96@gmail.com>
2022-07-18 17:42:20 +0300

    Create MainActivity.java
    task 1

---
+++ MainActivity.java
@@ -0,0 +1,34 @@
+vehicle app;
+
+/**
+Create a class  
+ vehicle
+**/
+
+/** 
+create attributes and create a constructor for all
+ color  
+ engine capacity
+ distance_per_litre 
+ fuel_cost_per_litre
+**/
+
+/**
+Create a method 
+Cost_of_Journey()
+uses the attributes distance_per_litre and fuel_cost_per_litre 
+**/
+
+/**
+create method sellcar() 
+price = Kshs 1m to Kshs 1.5m
+
+ if car= white;
+ engine capacity = 2000cc
+ 
+ else if
+ color=! white;
+ engine capacity = 2000cc, 
+ sell for 120/100*price. 
+ 
+**/
