686cc980117b3ce79f6df2a56476eb9ed9c8aaf1
Stephen Kimwe <stephenkimwe96@gmail.com>
2022-07-18 16:47:55 +0300

    Create MainActivity.java
    task

---
+++ MainActivity.java
@@ -0,0 +1,15 @@
+##reward app;
+
+##item 1, price, discount
+##item 2, price, discount
+##item 3, price, discount
+##item 4, price, discount
+  
+##read customer ID
+##read items bought
+
+##Calculate total items and total price for each item
+##Calculate discount
+
+##if four different items or more and price is above 1000 shortlist
+##open next activity display winner
