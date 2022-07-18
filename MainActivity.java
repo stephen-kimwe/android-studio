e330bdb74f4ea800c98319d77d1bfb11e9e44020
Stephen Kimwe <stephenkimwe96@gmail.com>
2022-07-18 16:31:32 +0300

    Create MainActivity.java
    dummy

---
+++ MainActivity.java
@@ -0,0 +1,16 @@
+##pointofsale app
+
+##item 1 button
+##item 2 button
+##item 3 button
+##item 4 button
+
+##button clicks corresponds to the quantity of item
+
+##Total button clicks per item and price per item displayed
+##Grand Total to be paid displayed
+
+##Discount 15%
+##Net to be paid after Grand total is discounted
+
+
