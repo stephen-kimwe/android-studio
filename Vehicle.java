8aaf20040c28e6c948b1e60cf921d14a9bed57b0
Stephen Kimwe <stephenkimwe96@gmail.com>
2022-07-19 22:58:53 +0300

    Create Vehicle.java

---
+++ Vehicle.java
@@ -0,0 +1,17 @@
+vehicle app;
+public class Vehicle{
+String color;
+int engine_capacity;
+
+ static void cost_of_journey(){
+ distance_per_litre*fuel_cost_per_litre; 
+}
+/** create method sellcar() price =1m to Kshs 1.5m**/ 
+static void sellcar()
+double price =1000000;
+double price_two=price*120/100;
+}
+if (color= white, engine_capacity= 2000){
+ system.out.print("sell car for : ", price);}
+ else{system.out.print("sell car for : ", price_two);}
+}
